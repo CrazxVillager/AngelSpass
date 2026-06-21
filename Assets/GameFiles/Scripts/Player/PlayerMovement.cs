abf94e32ed06b689c8d29f9f5a28bbf99ac40ad2
CrazyVillager <180844262+CrazxVillager@users.noreply.github.com>
2026-06-21 12:48:00 +0200

    implemented a character controller

--- Assets/GameFiles/Scripts/Player/PlayerMovement.cs
+++ Assets/GameFiles/Scripts/Player/PlayerMovement.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private PlayerData playerData;
+    
+    private Vector2 _moveDirection;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,11 +16,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(_moveDirection * (playerData.PlayerSpeed * Time.deltaTime));
     }
 
-    public void Move()
+    public void Move(InputAction.CallbackContext context)
     {
-        
+        _moveDirection = context.ReadValue<Vector2>();
     }
 }
